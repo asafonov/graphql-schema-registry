73ca5f46b09a7d578bacb30f61d0c0399d37181e
asafonov <me@asafonov.org>
2022-09-29 11:01:54 +0300

    update init migration

--- migrations/00_init.sql
+++ migrations/00_init.sql
@@ -1,47 +1,61 @@
+CREATE OR REPLACE FUNCTION update_updated_time_column()
+  RETURNS TRIGGER AS $$
+  BEGIN
+     NEW.updated_time = now(); 
+     RETURN NEW;
+  END;
+  $$ LANGUAGE 'plpgsql';
+
 CREATE TABLE IF NOT EXISTS persisted_queries
 (
-    `key`          varchar(100) NOT NULL,
+    key          varchar(100) NOT NULL,
     query        text         NOT NULL,
     is_active    int          NOT NULL DEFAULT '1',
-    updated_time datetime              DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-    added_time   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`key`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+    updated_time timestamp              DEFAULT NULL,
+    added_time   timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (key)
+);
+
+CREATE TRIGGER update_persisted_queries_updated_time BEFORE UPDATE
+        ON persisted_queries FOR EACH ROW EXECUTE PROCEDURE 
+        update_updated_time_column();
 
 CREATE TABLE IF NOT EXISTS services
 (
-    id           int unsigned NOT NULL AUTO_INCREMENT,
+    id           serial NOT NULL,
     name         varchar(255) NOT NULL DEFAULT '',
     is_active    int          NOT NULL DEFAULT '1',
-    updated_time datetime              DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-    added_time   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_time timestamp              DEFAULT NULL,
+    added_time   timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    UNIQUE KEY name (name)
-) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+    UNIQUE(name)
+);
 
-CREATE TABLE IF NOT EXISTS `schema`
+CREATE TRIGGER update_services_updated_time BEFORE UPDATE
+        ON services FOR EACH ROW EXECUTE PROCEDURE 
+        update_updated_time_column();
+
+CREATE TABLE IF NOT EXISTS schema
 (
-    id           int unsigned NOT NULL AUTO_INCREMENT,
-    service_id   int unsigned          DEFAULT NULL,
-    is_active    tinyint               DEFAULT '1' COMMENT 'If schema is deleted, this is set to 0',
-    type_defs    text COMMENT 'Graphql schema definition for specific service',
-    added_time   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time of first registration',
-    updated_time datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time of last registration OR deactivation',
+    id           serial NOT NULL,
+    service_id   int         DEFAULT NULL,
+    is_active    int               DEFAULT '1',
+    type_defs    text,
+    added_time   timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_time timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    KEY service_id (service_id),
     CONSTRAINT schema_ibfk_1 FOREIGN KEY (service_id) REFERENCES services (id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+);
 
 CREATE TABLE IF NOT EXISTS container_schema
 (
-    id         int unsigned NOT NULL AUTO_INCREMENT,
-    service_id int unsigned NOT NULL,
-    schema_id  int unsigned NOT NULL,
+    id         serial NOT NULL,
+    service_id int NOT NULL,
+    schema_id  int NOT NULL,
     version    varchar(100) NOT NULL DEFAULT '',
-    added_time datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    added_time timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    UNIQUE KEY service_id (service_id, version),
-    KEY schema_id (schema_id),
+    UNIQUE(service_id, version),
     CONSTRAINT container_schema_ibfk_1 FOREIGN KEY (service_id) REFERENCES services (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT container_schema_ibfk_2 FOREIGN KEY (schema_id) REFERENCES `schema` (id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+    CONSTRAINT container_schema_ibfk_2 FOREIGN KEY (schema_id) REFERENCES schema (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
